187c92c050846862d4e889bc829647a7adb37f31
Dave Goddard <dave@goddard.id.au>
2016-09-06 09:16:07 -0400

    Move needed variables to env variables

--- src/pg-service-broker.cr
+++ src/pg-service-broker.cr
@@ -1,32 +1,40 @@
 require "kemal"
 require "pg"
-require "yaml"
 require "json"
 
-CONFIG = JSON.parse(File.read("settings.json"))
-Kemal.config.add_handler Kemal::Middleware::HTTPBasicAuth.new("admin", "password")
+VCAP = JSON.parse(ENV["VCAP_APPLICATION"])
+CATALOG = JSON.parse(File.read("catalog.json")).tap do |catalog|
+  catalog["services"].each do |service|
+    service.as_h["id"] = Crypto::MD5.hex_digest("#{VCAP["application_id"]}:service")
+    service["plans"].each do |plan|
+      plan.as_h["id"] = Crypto::MD5.hex_digest("#{VCAP["application_id"]}:plan")
+    end
+  end
+end
+DATABASE_URL = "postgres://#{ENV["PG_USERNAME"]}:#{ENV["PG_PASSWORD"]}@#{ENV["PG_HOST"]}/postgres"
+DB = PG.connect(DATABASE_URL)
+
+
+Kemal.config.add_handler Kemal::Middleware::HTTPBasicAuth.new(ENV["USERNAME"], ENV["PASSWORD"])
 before_all do |env|
-  puts "Setting response content type"
   env.response.content_type = "application/json"
 end
 
 get "/v2/catalog" do |env|
-  CONFIG["catalog"].to_json
+  CATALOG.to_json
 end
 
 put "/v2/service_instances/:name" do |env|
   name = env.params.url["name"]
   name = "db" + Crypto::MD5.hex_digest(name)
+  # { "a" => "postgres://#{ENV["PG_USERNAME"]}:#{ENV["PG_PASSWORD"]}@#{ENV["PG_HOST"]}:5432/postgres", "name" => name }.to_json
 
   begin
-    c = CONFIG["pg"]
-    db = PG.connect("postgres://#{c["user"]:c["password"]@c["host"]:c["port"]/c["dbname"]")
-
-    db.exec("CREATE USER #{name} WITH PASSWORD '#{name}'")
-    db.exec("CREATE DATABASE #{name}")
-    db.exec("GRANT ALL PRIVILEGES ON DATABASE #{name} TO #{name}")
+    DB.exec("CREATE USER #{name} WITH PASSWORD '#{name}'")
+    DB.exec("CREATE DATABASE #{name}")
+    DB.exec("GRANT ALL PRIVILEGES ON DATABASE #{name} TO #{name}")
     env.response.status_code = 201
-    {"dashboard_url" => "postgres://#{name}:#{name}@c["host"]:c["port"]/#{name}"}.to_json
+    {"dashboard_url" => "postgres://#{name}:#{name}@#{ENV["PG_HOST"]}:5432/#{name}"}.to_json
   rescue e
     env.response.status_code = 502
     {"description" => e.message}.to_json
@@ -37,7 +45,7 @@ put "/v2/service_instances/:name/service_bindings/:sbid" do |env|
   name = env.params.url["name"]
   name = "db" + Crypto::MD5.hex_digest(name)
   env.response.status_code = 201
-  {"credentials" => "postgres://#{name}:#{name}@67.205.131.159:5432/#{name}"}.to_json
+  {"credentials" => { "uri" => "postgres://#{name}:#{name}@#{ENV["PG_HOST"]}:5432/#{name}"}}.to_json
 end
 
 delete "/v2/service_instances/:name/service_bindings/:sbid" do |env|
@@ -49,11 +57,8 @@ delete "/v2/service_instances/:name" do |env|
   name = "db" + Crypto::MD5.hex_digest(name)
 
   begin
-    c = CONFIG["pg"]
-    db = PG.connect("postgres://#{c["user"]:c["password"]@c["host"]:c["port"]/c["dbname"]")
-
-    db.exec("DROP DATABASE #{name}")
-    db.exec("DROP USER #{name}")
+    DB.exec("DROP DATABASE #{name}")
+    DB.exec("DROP USER #{name}")
     env.response.status_code = 201
     "{}"
   rescue e
@@ -61,5 +66,6 @@ delete "/v2/service_instances/:name" do |env|
     {"description" => e.message}.to_json
   end
 end
+
 
 Kemal.run
